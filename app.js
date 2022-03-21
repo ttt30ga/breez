8c64964a864bdbcc73d56710f174105b4a69c588
ttt30ga <promet30@protonmail.com>
2022-03-21 10:50:45 +0000

    Add temp solution for Geolocation error messages

--- app.js
+++ app.js
@@ -37,18 +37,19 @@ const initApp = () => {
 						});
 				},
 				(error) => {
+					let feedback = document.getElementById('feedback');
 					switch (error.code) {
 						case error.PERMISSION_DENIED:
-							x.innerHTML = 'User denied the request for Geolocation.';
+							feedback.innerHTML = 'User denied the request for Geolocation.';
 							break;
 						case error.POSITION_UNAVAILABLE:
-							x.innerHTML = 'Location information is unavailable.';
+							feedback.innerHTML = 'Location information is unavailable.';
 							break;
 						case error.TIMEOUT:
-							x.innerHTML = 'The request to get user location timed out.';
+							feedback.innerHTML = 'The request to get user location timed out.';
 							break;
 						case error.UNKNOWN_ERROR:
-							x.innerHTML = 'An unknown error occurred.';
+							feedback.innerHTML = 'An unknown error occurred.';
 							break;
 					}
 				}
